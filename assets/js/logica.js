c05f4418cc158256d5db734172eb17d797b335c5
Claudio Carrasco <86366533+claudio-hcn@users.noreply.github.com>
2025-06-03 20:47:46 -0400

    Update logica.js

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
   nombre = prompt(
     "Hola " +
       nombre +
-      ":\n Estás en mi página web, lamentablemente debo verificar \nque seas un ñoño del bootcamp de talento digital \nde lo contrario serás redireccionado a otra página\n\n DEBES ESCRIBIR EL NOMBRE DEL PROFE DEL CURSO (SE LLAMA ALONSO)"
+      ":\n Estás en mi página web, lamentablemente debo verificar \nque seas un estudiante del bootcamp de talento digital \nde lo contrario serás redireccionado a otra página\n\n DEBES ESCRIBIR EL NOMBRE DEL PROFE DEL CURSO (SE LLAMA ALONSO)"
   );
   cosa="nombre del profe del curso";
   nombre=validarNombre(nombre, cosa);
   nombre = nombre.toUpperCase();
   console.log(nombre);
-  if (nombre == "ALONSO" || nombre == "ALONSO") {
+  if (nombre == "ALONSO" || nombre == "ALONSO ") {
     alert("ahora si, bienvenido a mi sitio");
   } else {
     alert("largo de aquí");
